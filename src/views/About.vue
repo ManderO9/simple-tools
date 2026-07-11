192dc7b60d566a84da69f9aba45071ef68fdedc6
ManderO9 <h.chetoui@esi-sba.dz>
2026-07-11 18:13:26 +0000

    Deploying to gh-pages from  @ 4cf9ef62f7ac3a41d3d20722a6f1a82e35d02753 🚀

--- src/views/About.vue
+++
@@ -1,7 +0,0 @@
-
-
-<template>
-  <div class="container py-6">
-    <h1>you can quickly and easily test things here</h1>
-  </div>
-</template>
--- src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-
-
-<template>
-  <div class="container py-6">
-    <h1>Sample tools for the app</h1>
-  </div>
-</template>
