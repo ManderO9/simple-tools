b283406beef577ac1a18671406bacb5962c7a812
ManderO9 <hossem.chetoui@gmail.com>
2026-07-11 19:05:35 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <div class="container py-6">
+    <h1>you can quickly and easily test things here</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <div class="container py-6">
+    <h1>Sample tools for the app</h1>
+  </div>
+</template>
